eedf02cb16f1d3d574a7bd090906ebd742eb0fe5
kasundssc <kasun280@gmail.com>
2018-01-03 10:36:30 +0530

    Add files via upload

---
+++ EMR/DAL/Data/RelatedParty.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class RelatedParty
+    {
+        public int ID { get; set; }
+        public string RelatedPartyName { get; set; }
+        public string RelatedPartyAddress { get; set; }
+        public string MobileNoList { get; set; }
+        public byte Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+    }
+}
---
+++ EMR/DAL/DataServices/PatientService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using EMR.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL
+{
+    public class PatientService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PatientService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<RelatedParty> GetAllPatients()
+        {
+            IEnumerable<RelatedParty> patientList = _context.RelatedParty;
+            return patientList;
+        }
+    }
+}
